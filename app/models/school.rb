5a4c8bc8441fea76329532dada04d49d9db9abf2
= <kingliin814@gmail.com>
2016-02-05 11:18:07 +0800

    Style message show from school form & set validates for school

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,8 @@ class School < ActiveRecord::Base
 	has_many :grades
 	belongs_to :city
 	belongs_to :distance
+	validates :city_id, presence: true
+	validates :name, presence: true
+	validates :distance_id, presence: true
+
 end
