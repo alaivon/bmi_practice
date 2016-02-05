bc8886c5b00c96152df08f0e8c767af943cf1ce8
= <kingliin814@gmail.com>
2016-02-05 17:58:32 +0800

    Add CRUD to Student Controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,61 @@
 class StudentsController < ApplicationController
+
+	before_action :find_school
+	before_action :find_grade
+	before_action :find_student, except: [:new, :create]
+
+	def show
+		
+	end
+
+	def new
+		@student = @grade.student.new
+	end
+
+	def create
+		@student = @grade.student.build(student_params)
+		if @student.save
+			flash[:success] = "Congratulation! New member in your class."
+			redirect_to school_grade_path(@school, @grade)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @student.update(student_params)
+			flash[:success] = "Updated class member successfully."
+			redirect_to school_grade_path(@school, @grade)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		
+	end
+
+private
+	def find_school
+		@school = School.find(params[:school_id])	
+	end
+
+	def find_grade
+		@grade = @school.grades.find(params[:grade_id])
+	end
+
+	def find_student
+		@student = @grade.students.find(params[:id])
+	end
+
+	def student_params
+		params.require(:student).permit(:number, :name, :birth,
+																		:height, :weight, :bmi,
+																		:gender)
+	end
+
 end
