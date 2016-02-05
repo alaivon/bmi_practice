00c1cd8674a93596ddeee9b3ad535bc059e7db4e
= <kingliin814@gmail.com>
2016-02-05 16:34:15 +0800

    Style And Code CRUD of Grade controller and views

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,2 +1,63 @@
 class GradesController < ApplicationController
+	before_action :find_school
+	before_action :find_grade, except: [:index, :new, :create]
+
+	def index
+		@grades = @school.grades
+	end
+
+	def show
+		
+	end
+
+	def new
+		@grade = @school.grades.new
+	end
+
+	def create
+		@grade = @school.grades.build(grade_params)
+		if @grade.save
+			@grade.update(user_id: current_user.id)
+			flash[:success] = "Created a new grade successfully."
+			redirect_to school_grade_path(@school, @grade)
+		else
+			render :new
+		end
+
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @grade.update(grade_params)
+			flash[:success] = "Updated your grade successfully."
+			redirect_to school_grade_path(@school, @grade)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		
+	end
+
+private
+
+	def find_school
+		@school = School.find(params[:school_id])
+	end
+
+	def find_grade
+		@grade = @school.grades.find(params[:id])
+	end
+
+	def grade_params
+		params.require(:grade).permit(:name, :user_id, :school_id)
+	end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :schools
+  resources :schools do
+    resources :grades
+  end
   # You can have the root of your site routed with "root"
   root 'schools#index'
 
